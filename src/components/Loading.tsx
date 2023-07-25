82f460c227eea1c92bbefba1609b6bd35f9cdcf0
aslam <mohammedaslam4106@gmail.com>
2023-07-25 21:32:29 +0530

    removed the loading from the components

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-const Loading = ({children}) => {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
+// const Loading = ({children}) => {
+//   const [loading, setLoading] = useState(false);
+//   useEffect(() => {
+//     setLoading(true);
+//     setTimeout(() => {
+//       setLoading(false);
+//     }, 2000);
+//   }, []);
 
-  return (
-    <div className="container">
-      {loading ? (
-        <div className="loader-container">
-      	  <div className="spinner"></div>
-        </div>
-      ) : children}
-    </div>
-  );
-};
+//   return (
+//     <div className="container">
+//       {loading ? (
+//         <div className="loader-container">
+//       	  <div className="spinner"></div>
+//         </div>
+//       ) : children}
+//     </div>
+//   );
+// };
 
-export default Loading;
+// export default Loading;
